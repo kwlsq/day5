c4ec8440d30056f694004a7fe6f61df3a1600275
Vincentius <vincentiussss@gmail.com>
2025-02-07 14:26:31 +0700

    feat(game): implement hide words to menu

--- src/main/java/org/vincent/game/GameMenu.java
+++ src/main/java/org/vincent/game/GameMenu.java
@@ -10,19 +10,21 @@ public class GameMenu {
         String selectedDifficulty = scanner.nextLine();
 
         boolean isEasy = SelectDifficulty.isDifficultyEasy(selectedDifficulty);
-        final int maxAttempts = isEasy ? 10 : 20;
+        final int remainingAttempt = isEasy ? 10 : 20;
 
         //randomized easy words to be played. store the selected word in this class
-        WordUtils.getRandomizedWord(true);
-        String playedWord = WordUtils.getRandomizedWord(true);
-        String clue = WordsCollection.EASY_WORDS.get(playedWord);
-//        System.out.println(playedWord);
-//        System.out.println(clue);
+        WordUtils.getRandomizedWord(isEasy);
+        String playedWord = WordUtils.getRandomizedWord(isEasy);
+
+        String clue = isEasy ? WordsCollection.EASY_WORDS.get(playedWord) : WordsCollection.HARD_WORDS.get(playedWord);
 
         //hide 2 words to become underline and system out the result here
-
+        WordUtils.hideWords(playedWord,isEasy);
+        System.out.println("Clue: "+clue + ". Remaining attempts: " + remainingAttempt);
+        System.out.println(WordUtils.hideWords(playedWord,isEasy));
         //after shown to user
         //get the player 1st guess. compare using a method with inputs of stored selected words
+
         // and the guessed char.
 
         //update the hidden word with inputs of selected words, hidden words, guessed char
