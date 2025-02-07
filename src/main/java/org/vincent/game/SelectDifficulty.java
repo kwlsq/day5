843d787cbb1a591769417e5f5f610e880cf99abe
Vincentius <vincentiussss@gmail.com>
2025-02-07 13:17:17 +0700

    feat(game): create difficulty selection. it acts like selectRandomWord(). instead of choosing the word, player will choose difficulty (easy/hard)

---
+++ src/main/java/org/vincent/game/SelectDifficulty.java
@@ -0,0 +1,7 @@
+package org.vincent.game;
+
+public class SelectDifficulty {
+    public static boolean isDifficultyEasy(String selectedDifficulty){
+        return selectedDifficulty.equalsIgnoreCase("easy");
+    }
+}
