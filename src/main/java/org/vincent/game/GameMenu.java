9ff8b204bb93a3e97ebd7ea927816485f09ff81c
Vincentius <vincentiussss@gmail.com>
2025-02-07 14:00:07 +0700

    feat(game): implement select difficulty and randomized word in game menu

--- src/main/java/org/vincent/game/GameMenu.java
+++ src/main/java/org/vincent/game/GameMenu.java
@@ -3,30 +3,30 @@ package org.vincent.game;
 import java.util.Scanner;
 
 public class GameMenu {
-    public static void gameMenuInterface(){
+    public static void gameMenuInterface() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Word Guesser!");
         System.out.println("Choose the difficulty you want to play! (easy/hard)");
         String selectedDifficulty = scanner.nextLine();
 
-        if(SelectDifficulty.isDifficultyEasy(selectedDifficulty)){
-            //set max attempts to 10
-            final int maxAttempts = 10;
+        boolean isEasy = SelectDifficulty.isDifficultyEasy(selectedDifficulty);
+        final int maxAttempts = isEasy ? 10 : 20;
 
-            //randomized easy words to be played. store the selected word in this class
-            //hide 2 words to become underline and system out the result here
+        //randomized easy words to be played. store the selected word in this class
+        WordUtils.getRandomizedWord(true);
+        String playedWord = WordUtils.getRandomizedWord(true);
+        String clue = WordsCollection.EASY_WORDS.get(playedWord);
+//        System.out.println(playedWord);
+//        System.out.println(clue);
 
-            //after shown to user
-            //get the player 1st guess. compare using a method with inputs of stored selected words
-            // and the guessed char.
+        //hide 2 words to become underline and system out the result here
 
-            //update the hidden word with inputs of selected words, hidden words, guessed char
+        //after shown to user
+        //get the player 1st guess. compare using a method with inputs of stored selected words
+        // and the guessed char.
 
-            //display the game result. win or lose and reveal the word
-        } else {
-            //set max attempts to 20
-            final int maxAttempts = 20;
+        //update the hidden word with inputs of selected words, hidden words, guessed char
 
-        }
+        //display the game result. win or lose and reveal the word
     }
 }
