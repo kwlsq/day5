e2d171064b68560f9bed318d2389f97df5ee24b3
Vincentius <vincentiussss@gmail.com>
2025-02-07 14:00:19 +0700

    feat(game): call the game in main

--- src/main/java/org/vincent/Main.java
+++ src/main/java/org/vincent/Main.java
@@ -1,7 +1,10 @@
 package org.vincent;
 
+import org.vincent.game.GameMenu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        GameMenu.gameMenuInterface();
     }
 }
