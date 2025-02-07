22bec47068c90ac6dc6d3f03634ab80748e2a143
Vincentius <vincentiussss@gmail.com>
2025-02-07 13:38:26 +0700

    feat(game): add some interface and pseudocode for player interface

--- src/main/java/org/vincent/game/GameMenu.java
+++ src/main/java/org/vincent/game/GameMenu.java
@@ -1,4 +1,32 @@
 package org.vincent.game;
 
+import java.util.Scanner;
+
 public class GameMenu {
+    public static void gameMenuInterface(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Word Guesser!");
+        System.out.println("Choose the difficulty you want to play! (easy/hard)");
+        String selectedDifficulty = scanner.nextLine();
+
+        if(SelectDifficulty.isDifficultyEasy(selectedDifficulty)){
+            //set max attempts to 10
+            final int maxAttempts = 10;
+
+            //randomized easy words to be played. store the selected word in this class
+            //hide 2 words to become underline and system out the result here
+
+            //after shown to user
+            //get the player 1st guess. compare using a method with inputs of stored selected words
+            // and the guessed char.
+
+            //update the hidden word with inputs of selected words, hidden words, guessed char
+
+            //display the game result. win or lose and reveal the word
+        } else {
+            //set max attempts to 20
+            final int maxAttempts = 20;
+
+        }
+    }
 }
