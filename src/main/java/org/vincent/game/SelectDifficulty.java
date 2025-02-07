d34301a04f22f8d634b91d9d8f55a2bdeaa6703a
Vincentius <vincentiussss@gmail.com>
2025-02-07 13:38:00 +0700

    feat(game): add input validation for difficulty selector

--- src/main/java/org/vincent/game/SelectDifficulty.java
+++ src/main/java/org/vincent/game/SelectDifficulty.java
@@ -2,6 +2,13 @@ package org.vincent.game;
 
 public class SelectDifficulty {
     public static boolean isDifficultyEasy(String selectedDifficulty){
+        if(
+                !selectedDifficulty.equalsIgnoreCase("easy") ||
+                !selectedDifficulty.equalsIgnoreCase("hard")
+        ){
+            throw new IllegalArgumentException("Please choose between easy/hard");
+        }
+
         return selectedDifficulty.equalsIgnoreCase("easy");
     }
 }
